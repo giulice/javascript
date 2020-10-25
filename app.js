12d91ff7c20c474c972dd5684b3ba1d89d87aff4
giulice <giulicetto@gmail.com>
2020-10-25 13:10:11 -0300

    4-A

--- app.js
+++ app.js
@@ -98,6 +98,19 @@ console.log(meses.join("-"));
 meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 meses = meses.slice(4,11);
 
+/*********************************** 4- If Else ****************************************/
+
+/* A- Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(),
+si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” 
+y sino un alerta con el mensaje “Lower than 0,5”*/
+
+if (Math.random() >= 0.5){
+    alert("Greater than 0,5");
+}
+else{
+    alert("Lower than 0,5");
+}
+
 
 
 
