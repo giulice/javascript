c7d8a521980c428449ffec1b40aa3829f63f0f49
giulice <giulicetto@gmail.com>
2020-10-25 11:02:44 -0300

    2-A

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* 1- Variables y Operadores */
+/*********************************** 1- Variables y Operadores ****************************************/
 
 /* A- Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
 
@@ -18,6 +18,13 @@ guardando el resultado la suma una 3er variable (utilizar length).*/
 var a = "Giuliano"
 var b = "Cetto"
 var c = a.length + b.length
+
+/*********************************** 2- Strings ****************************************/
+
+/* Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).*/
+
+var a = "abcdefghij"
+a = a.toUpperCase()
 
 
 
