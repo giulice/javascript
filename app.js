36bdc7444882a4c854947f4c159158cfa48a3615
giulice <giulicetto@gmail.com>
2020-10-25 10:47:29 -0300

    1-A

---
+++ app.js
@@ -0,0 +1,9 @@
+/* 1- Variables y Operadores */
+
+/* A- Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
+
+var a = 1
+var b = 2
+var c = a + b
+
+console.log(c)
