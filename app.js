acac71f669c9207ca156740f1fe0a91f32bbac7a
giulice <giulicetto@gmail.com>
2020-10-25 11:48:21 -0300

    3-B

--- app.js
+++ app.js
@@ -65,10 +65,21 @@ var d = b + c;
 
 /*********************************** 2- Arrays ****************************************/
 
-/*Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+/* A- Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log)*/
 
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 console.log(meses[4] + " " + meses[10]);
+
+/* B- Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+
+meses = meses.sort()
+console.log(meses)
+
+
+
+
+
+
 
