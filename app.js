d9113777cb4a6a0cc2c2eb3d9f0a10a4e4888fff
giulice <giulicetto@gmail.com>
2020-10-25 13:59:39 -0300

    5-D

--- app.js
+++ app.js
@@ -169,6 +169,19 @@ for(var i=0;i<palabras.length;i++){
 }
 alert(sentence);
 
+/* D- Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición,
+es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9.
+Mostrar por la consola del navegador el al array final (utilizar console.log).*/
+
+var numeros = []
+
+for (var i=0;i<10;i++){
+    numeros[i]=i;
+}
+console.log(numeros);
+
+
+
 
 
 
