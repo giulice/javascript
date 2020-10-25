47b59c592262a38466c4b0aab60ab22bb851ab07
giulice <giulicetto@gmail.com>
2020-10-25 11:39:39 -0300

    2-F

--- app.js
+++ app.js
@@ -45,11 +45,23 @@ Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toL
 var a = "abcdEfGhiJ"
 var b = a.substring(0,1).toUpperCase() + a.substring(1).toLowerCase()
 
-/*Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco.
+/* E- Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco.
 Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
 
 var a = "abcd efghij"
 var b = a.indexOf(" ")
+
+/* F- Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
+Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas palabras en mayúscula
+y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
+
+var a = "giuliano cetto"
+var p = a.indexOf(" ")                                                          //Space position
+var b = a.substring(0,p)                                                        //first word
+var c = a.substring(p)                                                          //second word
+var b = b.substring(0,1).toUpperCase() + b.substring(1).toLowerCase()
+var c = c.substring(0,2).toUpperCase() + c.substring(2).toLowerCase()
+var d = b + c 
 
 
 
