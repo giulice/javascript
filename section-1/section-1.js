d73a87b1b913bb952b375106c6b86b088373b372
giulice <giulicetto@gmail.com>
2020-10-26 11:00:37 -0300

    Division of exercises into sections - 1

---
+++ section-1/section-1.js
@@ -0,0 +1,20 @@
+/*********************************** 1- Variables y Operadores ****************************************/
+
+/* A- Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
+
+var a = 1;
+var b = 2;
+var c = a + b;
+
+/* B- Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+
+var a = "Giuliano";
+var b = "Cetto";
+var c = a + b;
+
+/* C- Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado la suma una 3er variable (utilizar length).*/
+
+var a = "Giuliano";
+var b = "Cetto";
+var c = a.length + b.length;
